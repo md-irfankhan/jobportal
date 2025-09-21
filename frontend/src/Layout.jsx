cd1e7f7778d437dfd9d43b9d81522937645302d4
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-09-21 11:13:41 +0600

    nav added

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Nav from './components/Nav/Nav';
 
 const Layout = () => {
     return (
         <div>
+            <nav>
+                <Nav></Nav>
+            </nav>
             <div>
                 <Outlet></Outlet>
             </div>
